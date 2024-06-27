e97f23f523104936c70b0251b05fa84086e4e6bc
Vanessa Mendonça Leite <mend.vanessa75@gmail.com>
2024-06-27 01:00:02 -0400

    imagem aparecendo na tela de edt exercicios

--- DailyGaily/app/src/main/java/com/example/dailygaily/views/TelaEdtExercicios.java
+++ DailyGaily/app/src/main/java/com/example/dailygaily/views/TelaEdtExercicios.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +27,7 @@ public class TelaEdtExercicios extends AppCompatActivity {
     private TextView edtTipoExercicio, edtTempoExercicio;
     private long dbExercicioID;
     private LocalDatabase db;
+    private ImageView fotoExercicioEdt;
 
 
     @SuppressLint("WrongViewCast")
@@ -36,6 +39,7 @@ public class TelaEdtExercicios extends AppCompatActivity {
         edtTempoExercicio = findViewById(R.id.edtTempoExercicio);
         btnExcluirExercicio = findViewById(R.id.btnExcluirExercicio);
         btnSalvarExercicio = findViewById(R.id.btnSalvarExercicio);
+        fotoExercicioEdt = findViewById(R.id.fotoExercicioEdt);
 
         btnSalvarExercicio.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,6 +112,12 @@ public class TelaEdtExercicios extends AppCompatActivity {
         if (exercicio != null) {
             edtTipoExercicio.setText(exercicio.getTipoDeExercicio());
             edtTempoExercicio.setText(exercicio.getTempo().toString());
+
+            // Load image if available
+            if (exercicio.getFotoUri() != null) {
+                Uri photoURI = Uri.parse(exercicio.getFotoUri());
+                fotoExercicioEdt.setImageURI(photoURI);
+            }
         } else {
             Toast.makeText(this, "Erro: Exercício não encontrado", Toast.LENGTH_SHORT).show();
             finish();
